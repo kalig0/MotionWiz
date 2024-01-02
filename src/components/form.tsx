49663a75af2d4ac3b4bece1a4a1c15f6ed27bcd3
Daniel <mewregi@gmail.com>
2024-01-01 17:58:16 -0800

    changed class to className

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -26,7 +26,7 @@ const Form: React.FC<FormProps> = (props) => {
     loadingIcon = (
       <svg
         role="status"
-        class="inline h-5 w-5 animate-spin mr-2 text-gray-200 dark:text-gray-600 fill-blue-600"
+        className="inline h-5 w-5 animate-spin mr-2 text-gray-200 dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
